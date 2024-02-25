8aacbf0b78b939127408c6039756b84a8dea6863
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-02-25 22:37:00 +0530

    extra mode

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -56,7 +56,7 @@ export default function Body(props) {
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  the bulk of the card's content.Here you found every things
                 </p>
                 <a href="/" className="btn btn-primary">
                   Go somewhere
